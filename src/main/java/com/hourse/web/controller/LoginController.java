0579fcf227d4d685f78ffa802f8b37b25f0f7860
wufeng1210 <wufeng1210@qq.com>
2017-04-10 20:21:20 +0800

    新建java文件

---
+++ src/main/java/com/hourse/web/controller/LoginController.java
@@ -0,0 +1,7 @@
+package com.hourse.web.controller;
+
+/**
+ * Created by dell on 2017/4/10.
+ */
+public class LoginController {
+}
---
+++ src/main/java/com/hourse/web/model/Hourse.java
@@ -0,0 +1,7 @@
+package com.hourse.web.model;
+
+/**
+ * Created by dell on 2017/4/10.
+ */
+public class Hourse {
+}
