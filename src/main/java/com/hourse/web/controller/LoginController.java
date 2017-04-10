4ad05aadfbe60cb373a2826ece4b83817edf0a45
wufeng1210 <wufeng1210@qq.com>
2017-04-10 21:46:09 +0800

    springmvc框架结合log4j打印日志

--- src/main/java/com/hourse/web/controller/LoginController.java
+++ src/main/java/com/hourse/web/controller/LoginController.java
@@ -1,13 +1,17 @@
 package com.hourse.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
- * Created by dell on 2017/4/10.
+ * Created by wufeng on 2017/4/10.
  */
 @Controller
 public class LoginController {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoginController.class);
 
     @RequestMapping("index")
     public String index(){
