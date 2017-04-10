ff1daa2af6b6f5d2136d27c76bcc5de2e2833bf8
wufeng1210 <wufeng1210@qq.com>
2017-04-10 21:08:03 +0800

    springmvc框架搭建

--- src/main/java/com/hourse/web/controller/LoginController.java
+++ src/main/java/com/hourse/web/controller/LoginController.java
@@ -1,7 +1,16 @@
 package com.hourse.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by dell on 2017/4/10.
  */
+@Controller
 public class LoginController {
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
 }
