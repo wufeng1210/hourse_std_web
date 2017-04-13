8d141c815bb3dba9d5d252595f164676826813e1
wufeng1210 <wufeng1210@qq.com>
2017-04-14 00:15:03 +0800

    测试关于springmvc和jsp、freemaker的搭建

--- src/main/java/com/hourse/web/controller/LoginController.java
+++ src/main/java/com/hourse/web/controller/LoginController.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -24,8 +26,11 @@ public class LoginController {
     private IUserService iUserService;
 
     @RequestMapping("index")
-    public String index(){
-        return "index";
+    public ModelAndView index(HttpServletRequest request) {
+        ModelAndView modelAndView = new ModelAndView("index");
+//        modelAndView.addObject("securityName","1233");
+        request.setAttribute("securityName","12212");
+        return modelAndView;
     }
 
     @ResponseBody
